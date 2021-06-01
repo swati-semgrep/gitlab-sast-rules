95a10ccd48ee1154afa091061457161d8b1ca51b
anshuman <singhanshuman216@gmail.com>
2021-06-01 20:19:36 +0530

    Add rules+testcases B110,B112,B201

---
+++ python/bandit/B112/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+try:
+  do_some_stuff()
+except Exception:
+  continue
---
+++ python/bandit/B201/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    raise
+
+#bad
+app.run(debug=True)
+
+#okay
+app.run()
+app.run(debug=False)
+
+#unrelated
+run()
+run(debug=True)
+run(debug)
