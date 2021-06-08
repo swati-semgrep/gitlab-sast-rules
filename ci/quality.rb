40ae814cd7c241ab46e96d9f7862aa5c44388f68
julian <julianthome@pm.me>
2021-06-08 22:44:33 +0200

    quality

--- ci/quality.rb
+++ ci/quality.rb
@@ -7,7 +7,7 @@ require 'fileutils'
 module StringUtils
   def self.wrap(txt, col = 80)
     txt.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/,
-             "\\1\\3\n").gsub(/^# *$/, '')
+             "\\1\\3\n")
   end
 end
 
