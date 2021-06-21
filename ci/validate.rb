33596540fbb3ab5f8ffc6017b9bf440e84a957b7
Anshuman Singh <singhmanofficial@gmail.com>
2021-06-21 11:33:54 +0000

    Update gitlab-ci and validate.rb

--- ci/validate.rb
+++ ci/validate.rb
@@ -1,16 +1,15 @@
 require 'yaml'
 
 repeat = 0
+#Take out all ids from the ruleset and store in all_ruleids array
 all_ruleids = Dir.glob('**/rule-*.yml').map {|file| YAML.load(File.read(file))['rules'].first['id'] }
 # filter duplicate rule ids
-count_occurence = all_ruleids.tally
-duplicates = count_occurence.filter{ |ruleid, count| repeat > 1 }.map{ |ruleid,count| ruleid}
+duplicates = all_ruleids.tally.filter{ |ruleid, count| repeat > 1 }.map{ |ruleid,count| ruleid}
 
-puts all_ruleids
-puts duplicates
+#Showing duplicates to user.
 if duplicates.any?
  puts("duplicate ids: #{duplicates.join(",")}")
- exit(1)
+ exit(1) 
 end
 
 exit (0)
