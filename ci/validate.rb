b7d4c998524dc57d214148f019918ff86cc835be
Anshuman Singh <singhmanofficial@gmail.com>
2021-06-21 11:29:03 +0000

    remove psych use in validate.rb

--- ci/validate.rb
+++ ci/validate.rb
@@ -1,34 +1,16 @@
-require "yaml"
-require "psych"
+require 'yaml'
 
-repetition = 0
-all_ids = {} #accumulate ids in a hash
-Dir.glob('**/rule-*.yml').each do |file|  # checking through the directories for the yaml(rule) files
-    get_content = File.read(file) 
-    yaml_dict = YAML.load(get_content)
+repeat = 0
+all_ruleids = Dir.glob('**/rule-*.yml').map {|file| YAML.load(File.read(file))['rules'].first['id'] }
+# filter duplicate rule ids
+count_occurence = all_ruleids.tally
+duplicates = count_occurence.filter{ |ruleid, count| repeat > 1 }.map{ |ruleid,count| ruleid}
 
-    inside_file = Psych.parse_stream(Psych.dump(yaml_dict))
-    
-    inside_file.grep(Psych::Nodes::Mapping).each do |node|
-        node.children.each_slice(2) do |k, v|
-          case k.value
-          when 'id'    #Taking ids of each file and adding them it all_ids hash
-                if all_ids.include?(v.value) # Structure is -> {"the_id"=>number of occurence}
-                    all_ids[v.value]=add_ids[v.value]+1
-                else
-                    all_ids[v.value] = 1
-                end
-          end
-        end
-    end
+puts all_ruleids
+puts duplicates
+if duplicates.any?
+ puts("duplicate ids: #{duplicates.join(",")}")
+ exit(1)
 end
 
-all_ids.each do |key,value|  # Checking all_ids hash for occurences of each id
-    if value > 1                    # Check if any id occurs more than once.
-        repetition = repetition + 1 #If any id repeats
-    end
-end
-
-exit(-1) if repetition > 0 #Fail if ruleid is repeated(i.e., presence of duplicates)
-
-exit(0)
+exit (0)
