492459edd0fc406b4287b284f398fc458bc0e56e
Anshuman Singh <singhmanofficial@gmail.com>
2021-06-21 06:27:37 +0000

    change to psych to parse YAML

--- ci/validate.rb
+++ ci/validate.rb
@@ -1,30 +1,36 @@
 require "yaml"
+require "psych"
 
+array= []
 repetition = 0
 add_ids = {} #accumulate ids in a hash
 Dir.glob('**/rule-*.yml').each do |file|  # checking through the directories for the yaml files
     get_content = File.read(file) 
     yaml_dict = YAML.load(get_content)
 
-    yaml_dict['rules'].each do |key,value|  #Iterating through components in a rule
-        if key == "id"  #Taking out ids and storing them as values of add_ids hash
-            if add_ids.include?(value)
-                add_ids[value]+=1
-            else
-                add_ids[value]=1
-            end
+    ast = Psych.parse_stream(Psych.dump(yaml_dict))
+    
+    ast.grep(Psych::Nodes::Mapping).each do |node|
+        node.children.each_slice(2) do |k, v|
+          case k.value
+          when 'id'
+                if add_ids.include?(v.value)
+                    add_ids[v.value]=add_ids[v.value]+1
+                else
+                    add_ids[v.value] = 1
+                end
+          end
         end
     end
 end
 
+puts add_ids
 add_ids.each do |key,value|  # Checking add_ids hash for occurences of each id
     if value > 1
         repetition = repetition + 1 #If any id repeats
     end
 end
-
+puts repetition
 exit(-1) if repetition > 0 #Fail if ruleid is repeated
 
 exit(0)
-
-
