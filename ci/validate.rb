4e076d186bcee849df0b66a16fced1bb63eaa441
Anshuman Singh <singhmanofficial@gmail.com>
2021-06-21 06:36:28 +0000

    Update ci/validate.rb

--- ci/validate.rb
+++ ci/validate.rb
@@ -1,36 +1,34 @@
 require "yaml"
 require "psych"
 
-array= []
 repetition = 0
-add_ids = {} #accumulate ids in a hash
-Dir.glob('**/rule-*.yml').each do |file|  # checking through the directories for the yaml files
+all_ids = {} #accumulate ids in a hash
+Dir.glob('**/rule-*.yml').each do |file|  # checking through the directories for the yaml(rule) files
     get_content = File.read(file) 
     yaml_dict = YAML.load(get_content)
 
-    ast = Psych.parse_stream(Psych.dump(yaml_dict))
+    inside_file = Psych.parse_stream(Psych.dump(yaml_dict))
     
-    ast.grep(Psych::Nodes::Mapping).each do |node|
+    inside_file.grep(Psych::Nodes::Mapping).each do |node|
         node.children.each_slice(2) do |k, v|
           case k.value
-          when 'id'
-                if add_ids.include?(v.value)
-                    add_ids[v.value]=add_ids[v.value]+1
+          when 'id'    #Taking ids of each file and adding them it all_ids hash
+                if all_ids.include?(v.value) # Structure is -> {"the_id"=>number of occurence}
+                    all_ids[v.value]=add_ids[v.value]+1
                 else
-                    add_ids[v.value] = 1
+                    all_ids[v.value] = 1
                 end
           end
         end
     end
 end
 
-puts add_ids
-add_ids.each do |key,value|  # Checking add_ids hash for occurences of each id
-    if value > 1
+all_ids.each do |key,value|  # Checking all_ids hash for occurences of each id
+    if value > 1                    # Check if any id occurs more than once.
         repetition = repetition + 1 #If any id repeats
     end
 end
 puts repetition
-exit(-1) if repetition > 0 #Fail if ruleid is repeated
+exit(-1) if repetition > 0 #Fail if ruleid is repeated(i.e., presence of duplicates)
 
 exit(0)
