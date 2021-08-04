c0b3e3f91dcb263103263bc4f0d6f8e2f054b196
anshuman <singhanshuman216@gmail.com>
2021-08-04 14:36:37 +0530

    Add rule+testcase for char_TCHAR_wchar_t

---
+++ c/buffer/test-char_TCHAR.c
@@ -0,0 +1,11 @@
+// License: MIT (c) GitLab Inc.
+#include <locale.h>
+#include <wchar.h>
+#include <stdio.h>
+int main(){
+
+    wchar_t s = 0x2660;
+    char z = "Hello!";
+    TCHAR a=_T"Hello";
+    return 0;
+}
