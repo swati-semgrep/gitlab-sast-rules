e99b3b0e15961e28d94912befe298776a759476f
anshuman <singhanshuman216@gmail.com>
2021-08-04 22:14:22 +0530

    Apostrophe add

--- c/buffer/test-char_TCHAR.c
+++ c/buffer/test-char_TCHAR.c
@@ -2,10 +2,13 @@
 #include <locale.h>
 #include <wchar.h>
 #include <stdio.h>
-int main(){
 
-    wchar_t s = 0x2660;
-    char z = "Hello!";
-    TCHAR a=_T"Hello";
-    return 0;
+int main() {
+    static wchar_t s [100];
+    static wchar_t t [100] = {};
+    static char a [50];
+    static char b [50] = {};
+    static TCHAR c [20];
+    static TCHAR d [20]={}; 
+
 }
