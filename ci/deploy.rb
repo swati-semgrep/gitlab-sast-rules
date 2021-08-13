47214158e56b9f84a511b7270d6fc7a9a8031326
julian <julianthome@pm.me>
2021-08-13 16:57:20 +0200

    deployment strategy

--- ci/deploy.rb
+++ ci/deploy.rb
@@ -10,29 +10,41 @@ Dir.glob('mappings/*.yml').each do |mappings|
   prefix = File.basename(mappings, ".yml")
   dict = YAML.safe_load(File.read(mappings))
 
-  rulez = { }
-  counter = { }
+  rulez = {}
+  id2rules = {}
+  rule2ids = {}
+  ruleids = {}
+  rulefiles = []
 
   dict[prefix].each do |mapping|
-    id = mapping["id"]
-    mapping["rules"].each do |rule|
-      rulefiledict = YAML.safe_load(File.read("#{rule}.yml"))
-      rulefromfile = rulefiledict['rules'].first
-      remapped_prefix = "#{prefix}.#{id}"
-      counter[remapped_prefix] += 1 if counter.key?(remapped_prefix)
-      counter[remapped_prefix] = 1 unless counter.key?(remapped_prefix)
-      rulefromfile['id'] = "#{prefix}.#{id}-#{counter[remapped_prefix]}"
-      rulez[rulefromfile['id']] = rulefromfile
+    id = mapping['id']
+    ruleids[id] = ruleids.keys.size unless ruleids.key?(id)
+    id2rules[id] = [] unless id2rules.key?(id)
+    mapping['rules'].each do |rule|
+      rule2ids[rule] = [] unless rule2ids.key?(rule)
+      rule2ids[rule] << id
+      id2rules[id] << rule
+      rulefiles << rule
     end
   end
+  
+  rulefiles.sort!.uniq!
 
-  outdict = { 'rules' => [] }
-  rulez.each do |ruleid, rule|
-    ruleprefix = ruleid.gsub(/-[0-9]+$/, '')
-    id = counter[ruleprefix] == 1 ? ruleprefix : ruleid
-    rule['id'] = id
-    outdict['rules'] << rule
+  rulefiles.each do |rfil|
+    rulefiledict = YAML.safe_load(File.read("#{rfil}.yml"))
+    rulefromfile = rulefiledict['rules'].first
+    ids = rule2ids[rfil]
+    # generate a unique rule hash that makes it possible to map results
+    # back to the original anlyzer -- note that we have n:m mappings multiple
+    # native ids can be mapped to a collection of semgrep rules and vice versa
+    # every rule gets coordinates: original_rule_id-array index number
+    suffix = ids.map { |id| "#{id}-#{id2rules[id].find_index(rfil) + 1}" }.join('.')
+    newid = "#{prefix}.#{suffix}"
+    rulefromfile['id'] = newid
+    rulez[newid] = rulefromfile
   end
+
+  outdict = { 'rules' => rulez.values }
 
   formatted = AutoFormat.reformat_yaml('', outdict)
   puts("writing #{prefix}.yml")
