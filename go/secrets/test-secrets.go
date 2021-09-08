0b426d000584176380e38c0f2176e22e89461eec
Julian Thome <jthome@gitlab.com>
2021-09-08 13:11:05 +0000

    Initial go rule

---
+++ go/secrets/test-secrets.go
@@ -0,0 +1,13 @@
+// License: Apache 2.0
+// source: https://securego.io/docs/rules/g101.html
+
+package main
+
+import "fmt"
+
+func main() {
+    username := "admin"
+    var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
+
+    fmt.Println("Doing something with: ", username, password)
+}
