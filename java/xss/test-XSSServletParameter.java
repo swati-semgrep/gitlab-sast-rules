c20f06ca9b00e0c250fdc17183e572551ca3a804
Julian Thome <jthome@gitlab.com>
2022-03-23 09:56:32 +0000

    Fix test cases

--- java/xss/test-XSSServletParameter.java
+++ java/xss/test-XSSServletParameter.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class XSSServlet extends HttpServlet {
-
+public class XSSServletParameter extends HttpServlet {
     protected String danger(HttpServletRequest req) throws ServletException, IOException {
         String input1 = req.getParameter("input1");
         return input1;
