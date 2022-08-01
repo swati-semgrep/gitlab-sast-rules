7233fc2c9c0cc5dc33a1c51661c80f6b68a11c59
James Liu <jliu@gitlab.com>
2022-08-01 14:07:52 +1000

    wip: add C# command injection rule

---
+++ csharp/injection/test-CommandInjection.cs
@@ -0,0 +1,13 @@
+// License: LGPL-3.0 License (c) security-code-scan
+using System.Diagnostics;
+
+class Test
+{
+	static void Main()
+	{
+		var p = new Process();
+		p.StartInfo.FileName = "exportLegacy.exe";
+		p.StartInfo.Arguments = " -user " + Console.ReadLine() + " -role user";
+		p.Start();
+	}
+}
