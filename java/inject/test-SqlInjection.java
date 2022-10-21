4a67b39eafee02d86f3e507638f8358fae305e24
Julian Thome <jthome@gitlab.com>
2022-10-21 11:22:39 +0000

    SQLi FP case

--- java/inject/test-SqlInjection.java
+++ java/inject/test-SqlInjection.java
@@ -10,30 +10,41 @@ import javax.jdo.PersistenceManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.ArrayList;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SqlInjection {
+    private static final String CLIENT_FIELDS = "client_id, client_secret, resource_ids, scope, "
+            + "authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, "
+            + "refresh_token_validity, additional_information, autoapprove";
+
+    private static final String DEFAULT_INSERT_STATEMENT = "insert into oauth_client_details (" + CLIENT_FIELDS + ")"
+            + "values (?,?,?,?,?,?,?,?,?,?,?)";
+
+    private JdbcTemplate jdbcTemplate;
 
     public class UserEntity {
         private Long id;
         private String test;
+
         public String getTest() {
             return test;
         }
+
         public void setTest(String test) {
             this.test = test;
         }
+
         public Long getId() {
             return id;
         }
+
         public void setId(Long id) {
             this.id = id;
         }
     }
 
-    private static final PersistenceManagerFactory pmfInstance =
-            JDOHelper.getPersistenceManagerFactory("transactions-optional");
-
+    private static final PersistenceManagerFactory pmfInstance = JDOHelper
+            .getPersistenceManagerFactory("transactions-optional");
 
     public static PersistenceManager getPM() {
         return pmfInstance.getPersistenceManager();
@@ -46,7 +57,7 @@ public class SqlInjection {
 
         pm.newQuery("sql", "select * from Products where name = " + input);
 
-        //Test for false positive
+        // Test for false positive
 
         pm.newQuery("select * from Config");
 
@@ -59,17 +70,17 @@ public class SqlInjection {
     public void testJdoQueriesAdditionalMethodSig(String input) {
         PersistenceManager pm = getPM();
 
-        pm.newQuery(UserEntity.class,new ArrayList(),"id == "+ input); //Injection?
+        pm.newQuery(UserEntity.class, new ArrayList(), "id == " + input); // Injection?
 
-        pm.newQuery(UserEntity.class,new ArrayList(),"id == 1");
+        pm.newQuery(UserEntity.class, new ArrayList(), "id == 1");
 
-        pm.newQuery(UserEntity.class,"id == "+ input); //Injection?
+        pm.newQuery(UserEntity.class, "id == " + input); // Injection?
 
-        pm.newQuery(UserEntity.class,"id == 1");
+        pm.newQuery(UserEntity.class, "id == 1");
 
-        pm.newQuery((Extent) null,"id == "+input); //Injection?
+        pm.newQuery((Extent) null, "id == " + input); // Injection?
 
-        pm.newQuery((Extent) null,"id == 1");
+        pm.newQuery((Extent) null, "id == 1");
 
     }
 
@@ -83,4 +94,24 @@ public class SqlInjection {
         session.createQuery(input);
         CriteriaQuery<Object> cq = cb.createQuery(Object.class);
     }
+
+    public void good(String clientDetails) {
+        final String statementUsingConstants = "insert into oauth_client_details (" + CLIENT_FIELDS + ")"
+                + "values (?,?,?,?,?,?,?,?,?,?,?)";
+        jdbcTemplate.update(statementUsingConstants, clientDetails);
+    }
+
+    public void good2(String clientDetails) {
+        jdbcTemplate.update(DEFAULT_INSERT_STATEMENT, clientDetails);
+    }
+
+    public void bad(String clientDetails) {
+        String stmtUsingFuncParam = "test" + clientDetails + "test";
+        jdbcTemplate.update(stmtUsingFuncParam, clientDetails);
+    }
+
+    public void badInline(String clientDetails) {
+        jdbcTemplate.update("test" + clientDetails + "test", clientDetails);
+    }
+
 }
