bd6938c1e107ff95db96517e1bc82f14006be93b
Tomo Masakura <tomo.masakura@gmail.com>
2022-10-26 14:04:01 +0900

    add delete and put ... methods to csharp csrf rule

--- csharp/csrf/test-Csrf.cs
+++ csharp/csrf/test-Csrf.cs
@@ -9,7 +9,46 @@ class Csrf
         return null;
     }
 
+    [HttpDelete]
+    public ActionResult ControllerMethod(string input)
+    {
+        return null;
+    }
+
+    [HttpPatch]
+    public ActionResult ControllerMethod(string input)
+    {
+        return null;
+    }
+
+    [HttpPut]
+    public ActionResult ControllerMethod(string input)
+    {
+        return null;
+    }
+
     [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult ControllerMethod2(string input)
+    {
+        return null;
+    }
+
+    [HttpDelete]
+    [ValidateAntiForgeryToken]
+    public ActionResult ControllerMethod2(string input)
+    {
+        return null;
+    }
+
+    [HttpPatch]
+    [ValidateAntiForgeryToken]
+    public ActionResult ControllerMethod2(string input)
+    {
+        return null;
+    }
+
+    [HttpPut]
     [ValidateAntiForgeryToken]
     public ActionResult ControllerMethod2(string input)
     {
