a82c3d7086c87912ee674c343460890f713d8632
Vishwa Bhat <vbhat@gitlab.com>
2022-12-02 11:37:14 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- python/exception/test-try_except_pass.py
+++ python/exception/test-try_except_pass.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python
 # License: MIT (c) GitLab Inc.
 
-try:
-    v = 1
-except Exception:
-    logging.info("Info level logging")
-except Exception: pass
-except Exception as e:
-    pass
-except Exception as k:
-    logging.info("Info level logging")
+import logging
+
+def good():
+    try:
+        v = 1
+    except TypeError as k:
+        logging.info("Info level logging")
+    except Exception:
+        logging.info("Info level logging")
+
+def bad_basic():
+    try:
+        v = 1
+    except ValueError: pass
+    
+def bad_in_group():
+    try:
+        v = 1
+    except EOFError:
+        logging.info("Info level logging")
+    except ValueError: pass
+    except KeyError as e:
+        pass
+    except TypeError as k:
+        logging.info("Info level logging") 
+    
+def bad_with_finally():
+    try:
+        v = 1
+    except ValueError: pass
+    finally:
+        logging.info("blah")
+        
+def bad_nested():
+    try:
+        v = 0
+        try:
+            res = 1/v
+        except ZeroDivisionError:
+            pass
+    except Exception as e:
+        logging.error("blah", e)
+
 
 
