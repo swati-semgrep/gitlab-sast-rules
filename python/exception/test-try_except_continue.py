c9bb8b2e9a85b68f16f96fb5363a3ec207aa6c3e
Julian Thome <jthome@gitlab.com>
2022-12-02 13:21:01 +0100

    test cases

--- python/exception/test-try_except_continue.py
+++ python/exception/test-try_except_continue.py
@@ -7,48 +7,35 @@ def good():
     while True:
         try:
             v = 1
-        except TypeError as k:
+        except Exception as k:
             logging.info("Info level logging")
         except Exception:
             logging.info("Info level logging")
-
-def bad_basic(cond):
-    while cond:
-        try:
-            v = 1
-        except ValueError: continue
     
-def bad_in_group():
+def bad():
     condition = 10 > 0
     while condition:
         try:
             v = 1
-        except EOFError:
+        except Exception:
             logging.info("Info level logging")
-        except ValueError: continue
-        except KeyError as e:
+        except Exception: continue
+        except Exception as e:
             continue
-        except TypeError as k:
+        except Exception as k:
             logging.info("Info level logging") 
-    
-def bad_with_finally():
-    while True:
-        try:
-            v = 1
-        except ValueError: continue
         finally:
             logging.info("blah")
         
-def bad_nested():
+def bad2():
     keep_going = 1 > 0
     while keep_going:
         try:
             v = 0
             try:
                 res = 1/v
-            except ZeroDivisionError:
+            except Exception:
                 continue
         except Exception as e:
             logging.error("blah", e)
-
 
--- python/exception/test-try_except_pass.py
+++ python/exception/test-try_except_pass.py
@@ -1,48 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # License: MIT (c) GitLab Inc.
 
 import logging
 
 def good():
-    try:
-        v = 1
-    except TypeError as k:
-        logging.info("Info level logging")
-    except Exception:
-        logging.info("Info level logging")
-
-def bad_basic():
-    try:
-        v = 1
-    except ValueError: pass
-    
-def bad_in_group():
-    try:
-        v = 1
-    except EOFError:
-        logging.info("Info level logging")
-    except ValueError: pass
-    except KeyError as e:
-        pass
-    except TypeError as k:
-        logging.info("Info level logging") 
-    
-def bad_with_finally():
-    try:
-        v = 1
-    except ValueError: pass
-    finally:
-        logging.info("blah")
-        
-def bad_nested():
-    try:
-        v = 0
+    while True:
         try:
-            res = 1/v
-        except ZeroDivisionError:
+            v = 1
+        except Exception as k:
+            logging.info("Info level logging")
+        except Exception:
+            logging.info("Info level logging")
+    
+def bad():
+    condition = 10 > 0
+    while condition:
+        try:
+            v = 1
+        except Exception:
+            logging.info("Info level logging")
+        except Exception: pass
+        except Exception as e:
             pass
-    except Exception as e:
-        logging.error("blah", e)
-
-
+        except Exception as k:
+            logging.info("Info level logging") 
+        finally:
+            logging.info("blah")
+        
+def bad2():
+    keep_going = 1 > 0
+    while keep_going:
+        try:
+            v = 0
+            try:
+                res = 1/v
+            except Exception:
+                pass
+        except Exception as e:
+            logging.error("blah", e)
 
