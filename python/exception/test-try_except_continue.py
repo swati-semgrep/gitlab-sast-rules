83f99cce61a8f13171ae5033ce6fadda64dfe402
Julian Thome <jthome@gitlab.com>
2022-12-02 13:10:05 +0100

    test case

--- python/exception/test-try_except_continue.py
+++ python/exception/test-try_except_continue.py
@@ -1,13 +1,54 @@
 #!/usr/bin/env python3
 # License: MIT (c) GitLab Inc.
 
-x = 0
+import logging
 
-while x < 1:
-    try:
-        x = 1 
-    except Exception as e:
-        continue
-    except Exception: continue
-    except Exception:
-        x = 1
+def good():
+    while True:
+        try:
+            v = 1
+        except TypeError as k:
+            logging.info("Info level logging")
+        except Exception:
+            logging.info("Info level logging")
+
+def bad_basic(cond):
+    while cond:
+        try:
+            v = 1
+        except ValueError: continue
+    
+def bad_in_group():
+    condition = 10 > 0
+    while condition:
+        try:
+            v = 1
+        except EOFError:
+            logging.info("Info level logging")
+        except ValueError: continue
+        except KeyError as e:
+            continue
+        except TypeError as k:
+            logging.info("Info level logging") 
+    
+def bad_with_finally():
+    while True:
+        try:
+            v = 1
+        except ValueError: continue
+        finally:
+            logging.info("blah")
+        
+def bad_nested():
+    keep_going = 1 > 0
+    while keep_going:
+        try:
+            v = 0
+            try:
+                res = 1/v
+            except ZeroDivisionError:
+                continue
+        except Exception as e:
+            logging.error("blah", e)
+
+
